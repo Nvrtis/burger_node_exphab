20642c096f075328584debcc28516354c4859ad7
Nicholas Vrtis <nvav91@gmail.com>
2021-02-16 16:12:52 +0100

    updating heroku

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger")
+INSERT INTO burgers (burger_name) VALUES ("Beef Burger")
+INSERT INTO burgers (burger_name) VALUES ("Turkey Burger")
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", 1)
