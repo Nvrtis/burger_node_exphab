343f0295d06fa11911c3461b0451c54023e4d7dd
Nicholas Vrtis <nvav91@gmail.com>
2021-02-16 16:28:03 +0100

    updating heroku

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const router = require("./controllers/burger_controller")
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
